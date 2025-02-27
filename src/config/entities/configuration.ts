209f69a0575f9e460fa7e1c3b2bb7c1fe3f5dc3f
msvstj <savastijonok@gmail.com>
2025-02-27 22:33:11 +0200

    chore: update latest version

--- src/config/entities/configuration.ts
+++ src/config/entities/configuration.ts
@@ -6,7 +6,7 @@ import { randomBytes } from 'crypto';
 export default () => ({
   about: {
     name: 'safe-client-gateway',
-    version: process.env.APPLICATION_VERSION || 'v1.58.0',
+    version: process.env.APPLICATION_VERSION || 'v1.72.0',
     buildNumber: process.env.APPLICATION_BUILD_NUMBER,
   },
   accounts: {
